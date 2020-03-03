06ff078254e158f99afb3669f7293685e13f9dcc
Franco Queirolo <fqueiroloc@proveedor.intercorp.com.pe>
2020-03-03 18:26:49 -0500

    feat: observer attribute changes

--- tooltip.js
+++ tooltip.js
@@ -7,14 +7,21 @@ class Tooltip extends HTMLElement {
     this.shadowRoot.innerHTML = `
         <style>
             div {
-                background-color: black;
-                color: white;
-                position: absolute;
-                z-index: 10;
+              font-weight: normal;
+              background-color: black;
+              color: white;
+              position: absolute;
+              top: 1.5rem;
+              left: 0.75rem;
+              z-index: 10;
+              padding: 0.15rem;
+              border-radius: 3px;
+              box-shadow: 1px 1px 6px rgba(0,0,0,0.26);
             }
 
             :host(.important) {
               background: var(--color-primary, #ccc);
+              padding: 0.15rem;
             }
 
             :host-context(p) {
@@ -51,6 +58,19 @@ class Tooltip extends HTMLElement {
     tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
     this.shadowRoot.appendChild(tooltipIcon);
     this.style.position = 'relative';
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (oldValue === newValue) {
+      return;
+    }
+    if (name === 'text') {
+      this._tooltipText = newValue;
+    }
+  }
+
+  static get observedAttributes() {
+    return ['text'];
   }
 
   _showTooltip() {
